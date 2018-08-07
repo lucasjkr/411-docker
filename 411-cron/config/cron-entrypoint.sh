1abbbb4beda932a4146b16e6dcf3760e62e34599
lucasjkr <lucas@lucaskrupinski.com>
2018-08-06 23:09:35 -0400

    Cleanup
    No more need for Apache config files, alternative methods to run commands, etc.

--- 411-cron/config/cron-entrypoint.sh
+++ 411-cron/config/cron-entrypoint.sh
@@ -1,23 +1,4 @@
 #!/bin/bash
-#
-#touch /var/spool/cron/crontabs/root
-#
-#if [ -n "$1" ]; then
-#  args=("$@")
-#  argn=$#
-#
-#  for i in $(seq $argn)
-#  do
-#    echo "${args[$i-1]}" >> /var/spool/cron/crontabs/root
-#  done
-#fi
-#
-#cp /var/spool/cron/crontabs/root /tmp/temp.txt
-#printenv | cat - /tmp/temp.txt | tee /var/spool/cron/crontabs/root
-#chmod 600 /var/spool/cron/crontabs/root
-#
-#cron -f
-
 while [ true ]
 do
     /usr/bin/php /var/www/411/bin/cron.php
